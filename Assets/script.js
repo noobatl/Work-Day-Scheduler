b4df3022b25abfd8dd2278faa23e71bd90eec383
noobatl <info@noobusa.com>
2020-03-13 21:19:16 -0400

    completed index.html and style.css. Added hour style feature using js.

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,49 @@
-// PSEUDOCODE using a daily planner to create a schedule
 // DISPLAY current day at the top of the calendar
-// SCROLLING is presented with timeblocks for standard business hours
-// VIEWING timeblocks for that day each timeblock is color coded to indicate whether it is in the past (GRAY), present (RED), or future (GREEN)
+var currentDate = moment().format("MMMM Do YYYY, h:mm a");
+// console.log(currentDate);
+$("#currentDay").text(currentDate);
+// TIMEBLOCKS should know current hour to indicate past, present, and future blocks
+// USED capital H to change hours from am/pm to 24 hour clock
+// var currentHour = moment().format("H");
+// console.log (currentHour);
+// FOR TESTING PURPOSES:
+var currentHour = 15;
+var blockHour = parseInt($(this).attr("data-hour"));
+// COLORCODE row past (GRAY), present (RED), or future (GREEN) based on current hour
+$(".time-block").each(function() {
+  var blockHour = parseInt($(this).attr("data-hour"));
+  if (blockHour == currentHour) {
+    $(this).addClass("present");
+  } else if (blockHour < currentHour) {
+    $(this).addClass("past");
+  } else {
+    $(this).addClass("future");
+  }
+});
 // WHEN I click into a timeblock THEN I can enter an event
 // WHEN I click the save button for that timeblock THEN the text for that event is saved in local storage
+// $(".time-block").each(function() {
+//   var blockHour = parseInt($(this).attr("data-hour"));
+//   var toDoObject = {
+//     hour: blockHour,
+//     text: ""
+//   };
+//   .push(toDoObject);
+// });
+var todos = [];
+
+$(".time-block").each(function() {
+  var blockHour = parseInt($(this).attr("data-hour"));
+  var todoObject = {
+    hour: blockHour,
+    text: ""
+  };
+  todos.push(todoObject);
+  // Stringify and set "todos" key in localStorage to todos array
+});
+localStorage.setItem(".time-block", JSON.stringify(todos));
+
+$(".saveBtn").on("click", function (){
+    console.log("saved");
+})
 // WHEN I refresh the page THEN the saved events persist
